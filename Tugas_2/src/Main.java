a3f8d6b716f49d17b9f64165be5e0ca128f11bfc
Natalia <114990082+shanatta@users.noreply.github.com>
2023-08-08 08:06:26 +0700

    Create Main.java

---
+++ Tugas_2/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Data bioku = new Data();
+
+        bioku.biodata("Arni", "Perempuan", 16, "Johar");
+        bioku.biodata("Camela", "Perempuan", 17, "Banyumanik");
+        bioku.biodata("Devi", "Perempuan", 17, "Tlogosari");
+        bioku.biodata("Dona", "Perempuan", 16, "Rejosari");
+
+    }
+} 
