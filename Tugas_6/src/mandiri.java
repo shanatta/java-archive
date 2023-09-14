104fa6a0dc7a3a5d5706f11fd35a5b141f45b176
Natalia <114990082+shanatta@users.noreply.github.com>
2023-09-14 07:02:39 +0700

    Create mandiri.java

---
+++ Tugas_6/src/mandiri.java
@@ -0,0 +1,8 @@
+public class mandiri {
+    public static void main(String[] args) throws Exception {
+        String[] App = { "Keyboard", "Monitor", "RAM", "Casing", "CPU", "Printer" };
+        System.out.println("DAFTAR PERANGKAT KERAS KOMPUTER");
+        for (int i = 0; i < App.length; i++)
+            System.out.println(i + 1 + "." + App[i]);
+    }
+}
